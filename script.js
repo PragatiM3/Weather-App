33f91046f6400ce2d089881f6dd2613473471bd9
Pragati Moharana <pragatimoharana3@gmail.com>
2025-05-10 20:02:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+const button = document.querySelector(".button-89");
+button.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const place = document.getElementById("location").value;
+    function updateTemp(data){
+        const element=document.getElementById("result");
+        element.innerHTML = `Today's Temperature: ${data.current.temp_c} in celcius`;
+    }
+
+
+    const apiWeather = fetch(
+      `http://api.weatherapi.com/v1/current.json?key=93d44eb041e64aa9a8b121046252103&q=${place}&aqi=no`
+    );
+    apiWeather
+      .then((response) => response.json())
+      .then((data) => updateTemp(data));
+
+});
+
+
+    // const apiWeather = fetch(
+    //    `http://api.weatherapi.com/v1/current.json?key=93d44eb041e64aa9a8b121046252103&q=London&aqi=no`
+    //  );
+    // apiWeather
+    //   .then((response) => response.json())
+    //   .then((data) => console.log(data.current.temp_c));
